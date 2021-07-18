7440c054c13b6cc241f3da86607bef42b1b82119
Jess Thrysoee <jess@thrysoee.dk>
2021-07-18 16:13:01 +0200

    instal reload-services

--- install.sh
+++ install.sh
@@ -1,13 +1,36 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-{ # this ensures the entire script is downloaded #
+{
 
-install() {
-  echo "install"
+install_lego() {
+  local path="/usr/local/bin/lego"
 
+  curl -sSL "https://api.github.com/repos/go-acme/lego/releases/latest" \
+    | jq --unbuffered -r --arg arch "$(dpkg --print-architecture)" '.assets[].browser_download_url | select(.|endswith("linux_\($arch).tar.gz"))' \
+    | xargs curl -sSL \
+    | sudo tar -zx -C "${path%/*}" -- "${path##*/}"
+
+  chown root:root "$path"
+  chmod 755 "$path"
+
+  printf "installed: %s\n" "$path"
 }
 
-install
+install_reload_services() {
+  local path="/usr/local/bin/synology-letsencrypt-reload-services.sh"
 
-} # this ensures the entire script is downloaded #
+  sudo curl -sSL -o "$path" "https://raw.githubusercontent.com/JessThrysoee/synology-letsencrypt/master/synology-letsencrypt-reload-services.sh"
 
+  chown root:root "$path"
+  chmod 755 "$path"
+
+  printf "installed: %s\n" "$path"
+}
+
+
+install() {
+  install_lego
+  install_reload_services
+}
+
+}
