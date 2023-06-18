9a19185951ea83de8750173f0fda9bc51f0dbddd
Chris Rose <offline@offby1.net>
2023-06-18 07:44:05 -0700

    Amended for PR comments:
    - introduced getopt
    - made hook creation configurable
    - dedented the hook script

--- synology-letsencrypt.sh
+++ synology-letsencrypt.sh
@@ -1,11 +1,27 @@
 #!/bin/bash -e
 
-[[ $EUID == 0 ]] || { echo >&2 "This script must be run as root"; exit 1; }
-if [[ "${1:-xxx}" == "xxx" ]]; then
-    export LEGO_PATH="/usr/local/etc/synology-letsencrypt"
-else
-    export LEGO_PATH="/usr/local/etc/synology-letsencrypt/${1}"
-fi
+while getopts ":p:ch" opt; do
+    case $opt in
+        p) LEGO_PATH="$OPTARG" ;;
+        c) CREATE_HOOK=false ;;
+        h)
+            echo "Usage: $0 [options]"
+            echo "  -p <lego_path> The path where Lego will install your certs"
+            echo "  -c Suppress [c]reation of the hook scripts, if you have your own"
+            exit 0
+            ;;
+        :) echo "Error: -${OPTARG} requires an argument" >&2 ;;
+        \?) echo "Invalid iotion -$OPTARG" >&2 ;;
+    esac
+done
+
+LEGO_PATH=${LEGO_PATH:-/usr/local/etc/synology-letsencrypt}
+CREATE_HOOK=${CREATE_HOOK:-true}
+
+[[ $EUID == 0 ]] || {
+    echo >&2 "This script must be run as root"
+    exit 1
+}
 
 source "$LEGO_PATH/env"
 
@@ -13,31 +29,30 @@ cert_path="$LEGO_PATH/certificates"
 cert_domain="${DOMAINS[1]#\*.}"
 hook_path="$LEGO_PATH/hook"
 
-
 ## cert_id
 cert_id_path="$cert_path/$cert_domain.cert_id"
 if [[ ! -s $cert_id_path ]]; then
     mkdir -p "$cert_path"
-    /usr/local/bin/synology-letsencrypt-make-cert-id.sh > "$cert_id_path"
+    /usr/local/bin/synology-letsencrypt-make-cert-id.sh >"$cert_id_path"
 fi
 source "$cert_id_path"
 
-## install hook if necessary
+## install hook
 archive_path="/usr/syno/etc/certificate/_archive/$cert_id"
 if [[ ! -d $archive_path ]]; then
     mkdir -p "$archive_path"
 fi
 
-if [[ ! -x $hook_path ]]; then
+if [[ ${CREATE_HOOK} == true ]]; then
     cat >"$hook_path" <<EOF
-   #!/bin/bash
+#!/bin/bash
 
-   cp  $cert_path/$cert_domain.crt "$archive_path/cert.pem"
-   cp  $cert_path/$cert_domain.issuer.crt "$archive_path/chain.pem"
-   cat $cert_path/$cert_domain.crt $cert_path/$cert_domain.issuer.crt > $archive_path/fullchain.pem
-   cp  $cert_path/$cert_domain.key $archive_path/privkey.pem
+cp  $cert_path/$cert_domain.crt "$archive_path/cert.pem"
+cp  $cert_path/$cert_domain.issuer.crt "$archive_path/chain.pem"
+cat $cert_path/$cert_domain.crt $cert_path/$cert_domain.issuer.crt > $archive_path/fullchain.pem
+cp  $cert_path/$cert_domain.key $archive_path/privkey.pem
 
-   /usr/local/bin/synology-letsencrypt-reload-services.sh "$cert_id"
+/usr/local/bin/synology-letsencrypt-reload-services.sh "$cert_id"
 EOF
 
     chmod 700 "$hook_path"
@@ -56,7 +71,6 @@ fi
     --key-type "rsa4096" \
     --email "$EMAIL" \
     --dns "$DNS_PROVIDER" \
+    --dns.resolvers=8.8.8.8,8.8.4.4 \
     "${DOMAINS[@]}" \
     "${CMD[@]}" "$hook_path"
-
-
